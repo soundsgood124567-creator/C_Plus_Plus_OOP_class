93fa8431e13f72d5a69940c33e8b7d3ea4d2ade4
soundsgood124567 <soundsgood124567@gmail.com>
2026-07-16 21:12:46 +0700

    gym system vip

--- Final_Project(Gym_management_system)/gym_management.cpp
+++ Final_Project(Gym_management_system)/gym_management.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
+#include<conio.h>
 using namespace std;
 string user_name = "jason";
-string create_username, create_gender, create_coach, create_name,shift;
+string create_username, create_gender, create_coach, create_name,shift, search_name;
 int password = 1234;
-int vipornormal, create_password, vip_card, create_id;
+int vipornormal, create_password, vip_card, create_id, search_id, id_name;
 
 void login (){
 
@@ -29,14 +31,14 @@ else {
 }
 void vip_add(){
 	ofstream vip ("vipfile.txt");
-	cout<<"Enter name :";
+	cout<<"Enter username :";
 	cin>>create_username;
 	fflush(stdin);cin.clear();
 	cout<<"Enter password :";
 	cin>>create_password;
 	fflush(stdin);cin.clear();
 	cout<<"Ereate seccessfully"<<endl;
-	cout<<"Enter your name :";
+	cout<<"Enter your name(real name) :";
 	cin>>create_name;
 	fflush(stdin);cin.clear();
 	cout<<"Enter id :";
@@ -55,48 +57,93 @@ void vip_add(){
 	vip<<create_username<<create_password<<create_name<<create_id<<create_gender<<create_coach<<shift;
 	// <<vip_card
 }
-void VIP (){
-	cout<<"1. Add customer"<<endl;
-	cout<<"2. Show customer"<<endl;
-	cout<<"3. Update customer"<<endl;
-	cout<<"4. Delete customer"<<endl;
-	cout<<"5. Search customer"<<endl;
-	cout<<"Please choose one option :";
-	cin>>vipornormal;
-	switch (vipornormal){
-		case 1:
-			vip_add();
-			break;
-		case 2:
-			cout<<"read";
-			break;
-		case 3:
-			cout<<"update";
-			break;
-		case 4:
-			cout<<"delete";
-			break;
-		case 5:
-			cout<<"search";
-			break;
+void vip_read(){
+	ifstream vip("vipfile");
+	cout<<"VIP member list"<<endl;
+			cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
+			setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<endl;
+			cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
+			setw(20)<<create_coach<<setw(20)<<setw(20)<<endl;
+}
 
-			
-
+void search_use_id(){
+	cout<<"Enter VIP ID to search :";cin>>search_id;
+	fflush(stdin);cin.clear();
+	if(search_id == create_id){
+		cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
+			setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<endl;
+			cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
+			setw(20)<<create_coach<<setw(20)<<setw(20)<<endl;
 	}
 }
 
 
-void vip_read(){
-	ifstream vip("vipfile");
-	cout<<"VIP member list"<<endl;
-	cout<<"VIP name \t\t VIP id \t\t VIP gender \t\t VIP's coach \t\t VIP time"<<endl;
+
+
+void search_use_name(){
+	cout<<"Enter VIP username to search :";cin>>search_name;
+	fflush(stdin);cin.clear();
+	if(search_name == create_username){
+		cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
+			setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<endl;
+			cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
+			setw(20)<<create_coach<<setw(20)<<setw(20)<<endl;
+	}
 }
+
+
+
+void VIP() {
+
+    while (true) {
+        cout << "1. Add customer" << endl;
+        cout << "2. Show customer" << endl;
+        cout << "3. Search customer" << endl;
+        cout << "4. Delete customer" << endl;
+        cout << "5. Update customer" << endl;
+        cout << "6. Exit" << endl;
+        cout << "Please choose one option: ";
+        cin >> vipornormal;
+
+        switch (vipornormal) {
+            case 1:
+                vip_add();
+                break;
+
+            case 2:
+                vip_read();
+                break;
+
+            case 3:
+                cout<<"1. Search by id"<<endl;
+                cout<<"2. Search by name"<<endl;
+                cin>>id_name;
+                fflush(stdin);cin.clear();
+                switch (id_name)
+                break;
+
+            case 4:
+                cout << "Delete";
+                break;
+
+            case 5:
+                cout << "Update";
+                break;
+
+            case 6:
+                return;
+        }
+    }
+}
+
+
+
 	
 int main (){
 	
 	
 	login();	
-	cout<<"==========meun=========="<<endl;
+	cout<<"==========Meun=========="<<endl;
 	cout<<"1. VIP customer"<<endl;
 	cout<<"2. Normal customer"<<endl;
 	cout<<"Please choose one option :";
