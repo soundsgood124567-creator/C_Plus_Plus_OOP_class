163720c83a72d7e37cd47abb9f7e1033f8b953a9
soundsgood124567 <soundsgood124567@gmail.com>
2026-08-01 21:55:24 +0700

    homework

--- Final_Project(Gym_management_system)/gym_management.cpp
+++ Final_Project(Gym_management_system)/gym_management.cpp
@@ -3,78 +3,83 @@
 #include <iomanip>
 #include<conio.h>
 using namespace std;
+//vip varible
 string user_name = "jason";
 string create_username, create_gender, create_coach, create_name,shift, search_name,update_name,new_coach,new_name,new_shift,delete_name;
 int password = 1234;
 int vipornormal, create_password, vip_card, create_id, search_id, id_name, update_id, new_id,new_card_number,update_id_name,delete_id,del_id_name;
-bool found = false;                                                                                                                                                                                                          
+bool found = false;                                                                                                                                                                                               
+//normal customer
+
+string create_nor_name,create_nor_gender;
+int create_nor_age,create_nor_id,create_nor_card_id;
 
 void login (){
 
 cout<<"Enter username :";
 cin>>user_name;
 fflush(stdin);cin.clear();
-if (user_name == "jason"){
-    cout<<"Enter password :";   
-    cin>>password;
-    fflush(stdin);cin.clear();
-  if(password == 1234){
-    cout<<"login seccessfully"<<endl;
-  }
-  else{
-    cout<<"Wrong username or password.";
-    exit;
-    
-  }
-}
-else {
-    cout<<"Wrong username or password.";
-    exit;
-}
+	if (user_name == "jason"){
+	    cout<<"Enter password :";   
+	    cin>>password;
+	    fflush(stdin);cin.clear();
+	  if(password == 1234){
+	    cout<<"login seccessfully"<<endl;
+	  }
+	  else{
+	    cout<<"Wrong username or password.";
+	    exit;
+	    
+	  }
+	}
+	else {
+	    cout<<"Wrong username or password.";
+	    exit;
+	}
 }	
 void vip_add(){
 	getch();
 	system("cls");
-  ofstream vip("vipfile.txt",ios::app);
-  cout<<"Enter username :";
-  cin>>create_username;
-  fflush(stdin);cin.clear();
-  cout<<"Enter password :";
-  cin>>create_password;
-  fflush(stdin);cin.clear();
-  cout<<"Ereate seccessfully"<<endl;
-  cout<<"Enter your name(real name) :";
-  cin>>create_name;
-  fflush(stdin);cin.clear();
-  cout<<"Enter id :";
-  cin>>create_id;
-  fflush(stdin);cin.clear();
-  cout<<"Enter coach name :";
-  cin>>create_coach;
-  fflush(stdin);cin.clear();
-   cout<<"enter VIP card number :";
-   cin>>vip_card;
-  fflush(stdin);cin.clear();
-  cout<<"Enter shift (moring, afternoon, evening etc) :";
-  cin>>shift;
-  fflush(stdin);cin.clear();
-  cout<<"Create seccessfully";
-    vip << create_id << " " << create_name << " " << create_coach << " " << shift << "\n";
-    vip.close();
+	ofstream vip("vipfile.txt",ios::app);
+	cout<<"Enter username :";
+	cin>>create_username;
+	fflush(stdin);cin.clear();
+	cout<<"Enter password :";
+	cin>>create_password;
+	fflush(stdin);cin.clear();
+	cout<<"Ereate seccessfully"<<endl;
+	cout<<"Enter your name(real name) :";
+	cin>>create_name;
+	fflush(stdin);cin.clear();
+	cout<<"Enter id :";
+	cin>>create_id;
+	fflush(stdin);cin.clear();
+	cout<<"Enter coach name :";
+	cin>>create_coach;
+	fflush(stdin);cin.clear();
+	cout<<"enter VIP card number :";
+	cin>>vip_card;
+	fflush(stdin);cin.clear();
+	cout<<"Enter shift (moring, afternoon, evening etc) :";
+	cin>>shift;
+	fflush(stdin);cin.clear();
+	cout<<"Create seccessfully";
+	vip << create_id << " " << create_name << " " << create_coach << " " << shift << "\n";
+	vip.close();
 }
 void vip_read(){
-		getch();
+	getch();
 	system("cls");
-      ifstream vip("vipfile.txt");
-  while (vip>>create_id>>create_name>>create_coach>>shift){
-      cout<<"VIP member list"<<endl;
-      cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
-      setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<setw<<"Card number "<<endl;
-      cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
-      setw(20)<<create_coach<<setw(20)<<shift<<setw(20)<<vip_card<<endl;
-      vip.close();
-  }
-  vip.close();
+    ifstream vip("vipfile.txt");
+  	while (vip>>create_id>>create_name>>create_coach>>shift){
+		cout<<"VIP member list"<<endl;
+		cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
+		setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<setw<<"Card number "<<endl;
+		cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
+		setw(20)<<create_coach<<setw(20)<<shift<<setw(20)<<vip_card<<endl;
+		vip.close();
+  	}
+  	vip.close();
 }
 
 
@@ -85,75 +90,86 @@ void vip_read(){
 void search_use_id(){
 	getch();
 	system("cls");
-  ifstream vip("vipfile.txt");
-  cout<<"Enter VIP ID to search :";cin>>search_id;
-  fflush(stdin);cin.clear();
-  while(vip>>create_id>>create_name>>create_coach>>shift){
-    if(search_id == create_id){
-      cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
-      setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<endl;
-      cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
-      setw(20)<<create_coach<<setw(20)<<setw(20)<<endl;
-    }
-  }
-  vip.close();
+	ifstream vip("vipfile.txt");
+	cout<<"Enter VIP ID to search :";cin>>search_id;
+	fflush(stdin);cin.clear();
+	while(vip>>create_id>>create_name>>create_coach>>shift){
+	    if(search_id == create_id){
+	      cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
+	      setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<endl;
+	      cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
+	      setw(20)<<create_coach<<setw(20)<<setw(20)<<endl;
+	    }
+  	}	
+  	vip.close();
 
 }
-
-
-
 
 void search_use_name(){
 	getch();
 	system("cls");
-  ifstream vip("vipfile.txt");
-  cout<<"Enter VIP username to search :";cin>>search_name;
-  fflush(stdin);cin.clear();
-  while(vip>>create_id>>create_name>>create_coach>>shift){
-    if(search_name == create_username){
-      cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
-      setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<endl;
-      cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
-      setw(20)<<create_coach<<setw(20)<<setw(20)<<endl;
-    }
-  }
+	ifstream vip("vipfile.txt");
+	  cout<<"Enter VIP username to search :";cin>>search_name;
+	  fflush(stdin);cin.clear();
+	  while(vip>>create_id>>create_name>>create_coach>>shift){
+	    if(search_name == create_username){
+	      cout<<left<<setw(20)<<"VIP ID "<<setw(20)<<" VIP Name "<<
+	      setw(20)<<"VIP coach"<<setw(20)<<"VIP shift "<<endl;
+	      cout<<left<<setw(20)<<create_id<<setw(20)<<create_name<<
+	      setw(20)<<create_coach<<setw(20)<<setw(20)<<endl;
+	    }
+	  }
     vip.close();
 
 }
 
-
 void update_by_id(){
-	getch();
-	system("cls");
+  getch();
+  system("cls");
   ifstream vip("vipfile.txt");
   ofstream temp("temp.txt");
-  cout<<"Enter VIP ID to update :";cin>>update_id;
+  cout<<"Enter VIP id to update :";cin>>update_id;
   fflush(stdin);cin.clear();
-  while(vip>>create_id>>create_name>>create_coach>>shift>>vip_card){
-    if(update_id == create_id){
-		    cout<<"Enter new id :";cin>>new_id;
-		    fflush(stdin);cin.clear();
-		    cout<<"Enter new name :";cin>>new_name;
-		    fflush(stdin);cin.clear();
-		    cout<<"Enter new coach :";cin>>new_coach;
-		    fflush(stdin);cin.clear();
-		    cout<<"Enter new shift :";cin>>new_shift;
-		  	fflush(stdin);cin.clear();
-		    cout<<"Enter new card number :";cin>>new_card_number;
-		  	fflush(stdin);cin.clear();
 
-    }else{
-      cout<<"Wrong id.";
+  found = false;
+
+  while(vip>>create_id>>create_name>>create_coach>>shift){
+    if(update_id == create_id){
+      found = true;
+      cout<<"Enter new id :";cin>>new_id;
+      fflush(stdin);cin.clear();
+      cout<<"Enter new name :";cin>>new_name;
+      fflush(stdin);cin.clear();
+      cout<<"Enter new coach :";cin>>new_coach;
+      fflush(stdin);cin.clear();
+      cout<<"Enter new shift :";cin>>new_shift;
+      fflush(stdin);cin.clear();
+      cout<<"Enter new card number :";cin>>new_card_number;
+      fflush(stdin);cin.clear();
+
+      temp<<new_id<<" "<<new_name<<" "<<new_coach<<" "<<new_shift<<"\n";
     }
-    temp<<new_id<<" "<<new_name<<" "<<new_coach<<" "<<new_shift<<" "<<new_card_number;
-  
+    else{
+      temp<<create_id<<" "<<create_name<<" "<<create_coach<<" "<<shift<<"\n";
+    }
   }
-   
+
   vip.close();
   temp.close();
-  remove("vipfile.txt");
-   rename("temp.txt", "vipfile.txt");
+
+  if(found){
+    remove("vipfile.txt");
+    rename("temp.txt", "vipfile.txt");
+    cout<<"Update successfully"<<endl;
+  }
+  else{
+    remove("temp.txt");
+    cout<<"ID not found"<<endl;
+  }
 }
+
+
+
 void update_by_name(){
 	getch();
 	system("cls");
@@ -179,7 +195,7 @@ void update_by_name(){
       else{
         cout<<"wrong name";
     }
-        temp<<new_id<<" "<<new_name<<" "<<new_coach<<" "<<new_shift<<" "<<new_card_number;
+        temp<<new_id<<new_name<<new_coach<<new_shift<<new_card_number;
 
   }
    
@@ -241,6 +257,17 @@ void delete_by_name(){
 	remove("vipfile.txt");
 	rename("del.txt","vipfile.txt");
 }
+void nor_add(){
+	getch();
+	system("cls");
+	ofstream nor("nor.txt");
+	cout<<"Enter normal customor id : ";cin>>create_nor_id;fflsuh(stdin);cin.clear();
+	cout<<"Enter normal customor id : ";cin>>create_nor_id;fflsuh(stdin);cin.clear();
+	cout<<"Enter normal customor id : ";cin>>create_nor_id;fflsuh(stdin);cin.clear();
+	cout<<"Enter normal customor id : ";cin>>create_nor_id;fflsuh(stdin);cin.clear();
+	cout<<"Enter normal customor id : ";cin>>create_nor_id;fflsuh(stdin);cin.clear();
+	
+	}
 
 
 
@@ -317,7 +344,7 @@ void VIP() {
                      case 1:{
                      	
                        update_by_id();
-              break;
+              			break;
              }
              case 2:{
              
@@ -325,7 +352,6 @@ void VIP() {
               break;
              }
            }
-         
                 break;
 
             case 6:
@@ -333,9 +359,6 @@ void VIP() {
         }
     }
 }
-
-
-
   
 int main (){
   
